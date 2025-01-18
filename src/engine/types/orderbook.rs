b20af3150be448e2c13ed937b76cc63d2cd22dbd
Samyakt <isamyakt17@gmail.com>
2025-01-18 21:26:17 +0530

    refactor(orderbook): add Orderbook struct with initialization

---
+++ src/engine/types/orderbook.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+
+pub enum EntryType {
+    Sell,
+    Reverted,
+}
+
+pub struct IndividualEntry {
+    pub entry_type: EntryType,
+    pub quantity: u64,         
+}
+
+pub struct OrderEntry {
+    pub total: u64,
+    pub orders: HashMap<String, IndividualEntry>,          
+}
+
+pub struct Orderbook {
+    pub yes: HashMap<u64, OrderEntry>, 
+    pub no: HashMap<u64, OrderEntry>,
+}
+
+impl Orderbook {
+    pub fn new() -> Self {
+        Self {
+            yes: HashMap::new(),
+            no: HashMap::new(),
+        }
+    }
+}
