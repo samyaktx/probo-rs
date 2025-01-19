6e138133d5f05452df5357ec33bc18d1fcedb559
Samyakt <isamyakt17@gmail.com>
2025-01-19 23:22:42 +0530

    refactor(models): update balance management to support multi-user and multi-stock structures

--- src/engine/models/balance.rs
+++ src/engine/models/balance.rs
@@ -1,31 +1,49 @@
+use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 use crate::engine::types::balance::{DollarBalance, StockBalance};
 
+pub struct DollarBalanceType {
+    pub dollar_balance: HashMap<String, DollarBalance>,
+}
 
-pub fn dollar_balances() -> Arc<Mutex<DollarBalance>> {
-    static mut INSTANCE: Option<Arc<Mutex<DollarBalance>>> = None;
+impl DollarBalanceType {
+    pub fn new() -> Self {
+        Self {
+            dollar_balance: HashMap::new(),
+        }
+    }
+}
+
+pub struct StockBalanceType {
+    pub stock_balance: HashMap<String, HashMap<String, StockBalance>>,
+}
+
+impl StockBalanceType {
+    pub fn new() -> Self {
+        Self {
+            stock_balance: HashMap::new(),
+        }
+    }
+}
+
+pub fn dollar_balances() -> Arc<Mutex<DollarBalanceType>> {
+    static mut INSTANCE: Option<Arc<Mutex<DollarBalanceType>>> = None;
 
     unsafe {
         if INSTANCE.is_none() {
-            INSTANCE = Some(Arc::new(Mutex::new(DollarBalance {
-                balance: 0,
-                locked: 0,
-            })));
+            INSTANCE = Some(Arc::new(Mutex::new(DollarBalanceType::new())));
         }
         INSTANCE.as_ref().unwrap().clone()
     }
 }
 
-pub fn stock_balances() -> Arc<Mutex<StockBalance>> {
-    static mut INSTANCE: Option<Arc<Mutex<StockBalance>>> = None;
+pub fn stock_balances() -> Arc<Mutex<StockBalanceType>> {
+    static mut INSTANCE: Option<Arc<Mutex<StockBalanceType>>> = None;
 
     unsafe {
         if INSTANCE.is_none() {
-            INSTANCE = Some(Arc::new(Mutex::new(StockBalance {
-                yes: None,
-                no: None,
-            })));
+            INSTANCE = Some(Arc::new(Mutex::new(StockBalanceType::new())));
         }
         INSTANCE.as_ref().unwrap().clone()
     }
@@ -34,13 +52,11 @@ pub fn stock_balances() -> Arc<Mutex<StockBalance>> {
 pub fn reset_dollar_balance() {
     let dollar_balance = dollar_balances();
     let mut balance = dollar_balance.lock().unwrap();
-    balance.balance = 0;
-    balance.locked = 0;
+    balance.dollar_balance.clear();
 }
 
 pub fn reset_stock_balance() {
     let stock_balance = stock_balances();
     let mut balance = stock_balance.lock().unwrap();
-    balance.yes = None;
-    balance.no = None;
+    balance.stock_balance.clear();
 }
