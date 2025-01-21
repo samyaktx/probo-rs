7684f96d8020444506e60b11f3c30945396284b0
Samyakt <isamyakt17@gmail.com>
2025-01-21 18:44:02 +0530

    refactor(models): enhance OrderbookType with RefCell for fine-grained mutability and introduce OrderbookInstance for better instance management

--- src/engine/models/orderbook.rs
+++ src/engine/models/orderbook.rs
@@ -1,34 +1,37 @@
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 use crate::engine::types::orderbook::Orderbook;
 
-
+#[derive(Default)]
 pub struct OrderbookType {
-    pub orderbook: HashMap<String, Orderbook>,
+    pub orderbook: HashMap<String, RefCell<Orderbook>>,
 }
 
-impl OrderbookType {
-    pub fn new() -> Self {
-        Self {
-            orderbook: HashMap::new(),
+#[derive(Clone)]
+pub struct OrderbookInstance {
+    pub orderbook_instance: Arc<Mutex<OrderbookType>>,
+}
+
+impl OrderbookInstance {
+    pub fn orderbook_instance() -> Self {
+        static mut INSTANCE: Option<OrderbookInstance> = None;
+
+        unsafe {
+            if INSTANCE.is_none() {
+                INSTANCE = Some(OrderbookInstance { orderbook_instance: Arc::new(Mutex::new(OrderbookType::default())) });
+            }
+            INSTANCE.as_ref().unwrap().clone()
         }
+    }
+    pub fn reset_orderbook() -> Self {
+        let orderbook = Self::orderbook_instance();
+
+        let orderbook_type = orderbook.orderbook_instance.clone();
+        orderbook_type.lock().unwrap().orderbook.clear();
+        orderbook
     }
 }
 
-pub fn orderbook() -> Arc<Mutex<OrderbookType>> {
-    static mut INSTANCE: Option<Arc<Mutex<OrderbookType>>> = None;
 
-    unsafe {
-        if INSTANCE.is_none() {
-            INSTANCE = Some(Arc::new(Mutex::new(OrderbookType::new())));
-        }
-        INSTANCE.as_ref().unwrap().clone()
-    }
-}
-
-pub fn reset_orderbook() {
-    let orderbook = orderbook();
-    let mut orderbook_type = orderbook.lock().unwrap();
-    orderbook_type.orderbook.clear();
-}
