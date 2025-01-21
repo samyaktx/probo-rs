b151a0d046694edae8c3e4c9ff394eacf680afa4
Samyakt <isamyakt17@gmail.com>
2025-01-21 18:47:38 +0530

    refactor(types): add Debug and PartialEq traits to StockOption and StockBalance for improved debugging and comparisons

--- src/engine/types/balance.rs
+++ src/engine/types/balance.rs
@@ -3,12 +3,13 @@ pub struct DollarBalance {
     pub locked: u64,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct StockOption {
     pub quantity: u64, 
     pub locked: u64,
 }
 
-#[derive(Default)] // Optional: Makes it easy to initialize with default values
+#[derive(Default, Debug,PartialEq)] // Optional: Makes it easy to initialize with default values
 pub struct StockBalance {
     pub yes: Option<StockOption>, // Optional fields are represented as `Option`
     pub no: Option<StockOption>,
